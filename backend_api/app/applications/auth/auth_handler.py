4995353b960449f3cf6379d61e3a786f63eb0db1
Andrey <Andrey827541@gmail.com>
2025-07-18 17:00:36 +0300

    verify user

--- backend_api/app/applications/auth/auth_handler.py
+++ backend_api/app/applications/auth/auth_handler.py
@@ -1,7 +1,8 @@
 from settings import settings
-from fastapi import APIRouter, Depends
+from applications.auth.password_handler import PasswordEncrypt
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-
+from applications.users.crud import get_user_by_email
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -14,7 +15,21 @@ class AuthHandler:
         self.algorithm = settings.JWT_ALGORITHM
 
     async def get_login_token_pairs(self, data: OAuth2PasswordRequestForm, session: AsyncSession):
-        pass
+        user_email = data.username
+        user_password = data.password
+        user = await get_user_by_email(user_email, session)
 
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='User not found'
+            )
+
+        is_valid_password = await PasswordEncrypt.verify_password(user_password, user.hashed_password)
+        if not is_valid_password:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail='Incorrect password'
+            )
 
 auth_handler = AuthHandler()
