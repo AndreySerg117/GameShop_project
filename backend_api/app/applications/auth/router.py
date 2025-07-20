89541dda8f6ea5b519d65ce215d172bbd1f601f1
Andrey <Andrey827541@gmail.com>
2025-07-20 15:00:21 +0300

    verify user changes

--- backend_api/app/applications/auth/router.py
+++ backend_api/app/applications/auth/router.py
@@ -17,3 +17,8 @@ async def user_login(
 ):
     token_pair = await auth_handler.get_login_token_pairs(data, session)
     return token_pair
+
+
+@router_auth.get("/get-my-info")
+async def get_my_info():
+    pass
